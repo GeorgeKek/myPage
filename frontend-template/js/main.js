41592259b892c0b85c62c9efd94b9effa745ff95
Aczamaz Valiev <george.kozaev@gmail.com>
2020-03-12 17:45:42 +0300

    Create cards

--- frontend-template/js/main.js
+++ frontend-template/js/main.js
@@ -12,17 +12,65 @@ $(document).ready(function()
  * основной объект
  * @type {object}
  */
-var o2 =
+const o2 =
 {
-	/**
+	/*
 	 * вызов функций, которые должны запускаться при загрузке страницы
 	 */
 	init: function()
 	{
-		console.log('стартовый шаблон');
-		var item = 'alahsssa sadsf';
-
-		var object = { item };
-		console.log(object);
+		this.slider();
 	},
+	slider()
+	{
+		let tapSlider = $('.strategy__line-slider')[0];
+		let noUiYear = $('.strategy__count:last')[0];
+		let noUiMonth = $('.strategy__count:first')[0];
+		let summInvest = $('.strategy__item:eq(0)');
+		let heightInvest = $('.strategy__change:first');
+		let incomeInvest = $('.strategy__change:last');
+		let maxIncome = $('.strategy__item:eq(9)');
+		let countFormat = wNumb({
+				decimals: 0,
+				thousand: ' '
+			});
+		noUiSlider.create(tapSlider, {
+			start: 400000,
+			behaviour: 'tap',
+			connect: [true, false],
+			step: 1000,
+			range: {
+				'min': 50000,
+				'max': 1000000
+			},
+			format: wNumb({
+				decimals: 0,
+				thousand: ' '
+			}),
+		});
+
+		tapSlider.noUiSlider.on('update', function (values) {
+			noUiYear.value = values.join();
+			noUiMonth.value = countFormat.to(Math.round(parseInt(countFormat.from(noUiYear.value)) / 12));
+			summInvest.text(noUiYear.value);
+			heightInvest.text(countFormat.to(Math.round(parseInt(countFormat.from(noUiYear.value)) * 0.25)));
+			incomeInvest.text(countFormat.to(Math.round(parseInt(countFormat.from(noUiYear.value)) * 0.05)));
+			maxIncome.text('=' + countFormat.to(Math.round(parseInt(countFormat.from(noUiYear.value)) * 1.30)));
+
+		});
+		console.log();
+	}
+
 }
+// console.log('hello');
+
+// var stepSlider = document.getElementById('strategy__line-slider');
+
+// noUiSlider.create(stepSlider, {
+//     start: 300,
+//     step: 100,
+//     range: {
+//         'min': [0],
+//         'max': [1000]
+//     }
+// });
